4e585903003618288bf057b52e7ed4781a100b95
Osas706 <92319443+Osas706@users.noreply.github.com>
2023-10-25 16:47:02 +0100

    Weather App

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<script>
+import Weather from './components/weather.vue'
+
+export default (await import('vue')).defineComponent({
+  name:'App',
+  components: {
+    Weather
+  },
+  data(){
+    return{
+      city: '',
+      showWeather: false
+    }
+  },
+  methods:{
+    async searchWeather(){
+      this.showWeather = false;
+      await this.$nextTick();
+      this.showWeather = true;
+    }
+  }
+})
+
+</script>
+
+<template>
+  <div class="app">
+    <div class="header container h-10 p-5">
+      <h1 class="mb-5">Weather App</h1>
+      <div class="d-flex justify-content-center h-80">
+        <div class="searchbar w-50 mx-2">
+          <input type="text" class="input form-control" v-model="city" placeholder="Enter a city">
+        </div>
+        <button class="btn-search btn btn-primary" @click="searchWeather">
+          Search 
+        </button>
+      </div>
+    </div>
+    <br>
+    
+    <Weather :city="city" v-if="showWeather"/>
+  </div>
+</template>
+
+<style>
+body{
+  background-color: #121212 !important;
+  margin: 0;
+  padding: 0;
+}
+
+.app{
+  width: 90vw;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+.header{
+  background-color: #212730;
+  border-radius: 20px;
+  color: #fff;
+  text-align: center;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+.btn{
+  background-image: linear-gradient(to right, cyan, magenta);
+  height: 2.4rem;
+}
+
+
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,215 @@
+<script>
+import axios from 'axios'
+import DaysWeather from "./daysWeather.vue";
+
+export default (await import("vue")).defineComponent({
+  name: "myWeather",
+  components: {
+    DaysWeather,
+  },
+  props:{
+    city: String,
+  },
+  data(){
+    return{
+      cityname: this.city,
+
+      temperature: null,
+      description: null,
+      iconUrl: null,
+      date: null,
+      time: null,
+      monthNames: ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'],
+
+      sea_level: null,
+      wind: null,
+      Country: null,
+      humidity: null,
+
+    }
+  },
+  methods: {
+    changeLocation(){
+      window.location.reload();
+    }
+  },
+  async created() {
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=matric&appid=1384d68bd2647f38b39bf22eb8ec5d28`)
+
+    const weatherData = response.data;
+    this.temperature = Math.round(weatherData.main.temp);
+    this.description = weatherData.weather[0].description;
+    this.name = weatherData.name;
+    this.iconUrl = `https://api.openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const d = new Date();
+    this.date = d.getDate() + '-' + this.monthNames[d.getMonth()] + '-' + d.getFullYear();
+    this.time = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+
+    this.Country = weatherData.sys.country;
+    this.wind = weatherData.wind.speed; 
+    this.sea_level = weatherData.main.sea_level;
+    this.humidity = weatherData.main.humidity
+
+    console.log(weatherData);
+  },
+});
+</script>
+
+<template>
+  <div class="container p-0">
+    <div class="d-flex">
+      <div class="card main-div w-100" id="main-div">
+        <div class="p-3">
+          <h2 class="mb-1 day">Today</h2>
+          <p class="text-light date mb-0">{{ date }}</p>
+          <small>{{ time }}</small>
+          <h2 class="place">
+            . {{ name }} <small>{{ Country }}</small>
+          </h2>
+          <div class="temp">
+            <h1 class="weather-temp">{{ temperature }}&deg;</h1>
+            <h2 class="text-light">{{ description }} <img :src="iconUrl"></h2>
+          </div>
+        </div>
+      </div>
+
+      <div class="card card-2 w-100" id="card-2">
+        <table class="m-4">
+          <tbody>
+            <tr>
+              <th>Sea level</th>
+              <td v-if="sea_level > 0">{{ sea_level }}</td>
+              <td v-else>__</td>
+            </tr>
+            <tr>
+              <th>wind</th>
+              <td>{{ wind }}</td>
+            </tr>
+            <tr>
+              <th>Humidity</th>
+              <td>{{ humidity }}</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <DaysWeather :cityname="cityname" />
+
+        <div id="div-form" class="d-flex m-3 justify-content-center">
+          <form action="">
+            <input
+              type="button"
+              value="Change Location"
+              class="btn change-btn btn-primary"
+              @click="changeLocation"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+
+.card{
+   background-color: #343d4b;
+}
+
+.weather-temp {
+  margin: 0;
+  font-weight: 700;
+  font-size: 4em;
+}
+
+h2.mb-1.day {
+  font-size: 3rem;
+  font-weight: 400;
+}
+
+#main-div {
+  border-radius: 20px;
+  color: #fff;
+  background-image: url(../img/skyyyy.jpg); 
+  background-size: cover;
+  background-position: center;
+  background-blend-mode: overlay;
+  background-color: rgba(0, 0, 0, 0.5);
+  background-repeat: no-repeat;
+
+}
+
+.temp {
+  position: absolute;
+  bottom: 0;
+}
+
+#main-div:hover {
+  transform: scale(1.02);
+  transition: transform 0.5s ease;
+  z-index: 1;
+}
+
+#card-2 {
+  background-color: #212730;
+  border-radius: 20px;
+}
+
+/**************************************/
+.card-details {
+  margin-left: 19px;
+}
+
+.h1_left {
+  position: absolute;
+  bottom: 25px;
+  left: 16px;
+  font-size: 3vw;
+  line-height: 1.2;
+}
+
+.h3_left {
+  position: absolute;
+  left: 16px;
+  font-size: 2vw;
+  line-height: 0.5;
+}
+
+.h3_left small {
+  font-size: 1rem;
+}
+
+table {
+  position: relative;
+  left: 15px;
+  border-collapse: separate;
+  border-spacing: 15px;
+  width: 85%;
+  text-align: left;
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+th,
+td {
+  font-size: 10px;
+  color: #fff;
+}
+
+td {
+  text-align: right;
+}
+
+table,
+tr:hover {
+  color: red;
+}
+
+.change_btn {
+  background-image: linear-gradient(to right, cyan, magenta);
+}
+
+.change_btn:hover {
+  transform: scale(0.9);
+  transition: transform 0.1s ease;
+}
+</style>
